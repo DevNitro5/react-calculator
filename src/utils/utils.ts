a437ef14c5e37f1320f9df79b795f58fff3e7e32
DevNitro5 <anishchhetri196@gmail.com>
2023-01-04 08:28:52 +0000

    add RemovesNullValue type

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -11,19 +11,19 @@ export function formatOperand(operand: string | null) {
   return `${INTEGER_FORMATTER.format(+integer)}.${decimal}`;
 }
 
-// type RemovesNullValue<T> = {
-//   readonly [k in keyof T]: Exclude<T[k], null>;
-// };
+export type RemovesNullValue<T> = {
+  readonly [k in keyof T]: Exclude<T[k], null>;
+};
 export function evaluate({
   leftOperand,
   operator,
   rightOperand,
-}: CalculatorState) {
+}: RemovesNullValue<CalculatorState>) {
   const prev = parseFloat(leftOperand || "");
   const current = parseFloat(rightOperand || "");
 
   if (isNaN(prev) || isNaN(current)) return "";
-  let computation: number;
+
   switch (operator) {
     case "+":
       return prev + current;
